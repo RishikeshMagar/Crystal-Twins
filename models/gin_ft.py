9bd67aff672d57ab5183bb7b8224e65d37587ee3
RishikeshMagar <43094762+RishikeshMagar@users.noreply.github.com>
2022-08-17 15:41:42 -0400

    GINET - SSL

---
+++ models/gin_ft.py
@@ -0,0 +1,171 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.nn import Linear, LayerNorm, ReLU
+
+from torch_scatter import scatter
+# from torch_geometric.nn import GINEConv
+from torch_geometric.nn import MessagePassing
+from torch_geometric.utils import add_self_loops, degree, softmax
+from torch_geometric.nn import global_add_pool, global_mean_pool, global_max_pool
+
+
+# ATOM_LIST = list(range(1,119))
+# ATOM_LIST = [
+#     1, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
+#     31, 32, 33, 34, 35, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 55, 56, 57, 
+#     58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 
+#     83, 89, 90, 91, 92, 93, 94
+# ]
+ATOM_LIST = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 
+    57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 
+    83, 89, 90, 91, 92, 93, 94
+]
+ATOM_LIST = list(range(1,100))
+num_atom_type = len(ATOM_LIST) + 1 # including the extra mask tokens
+num_class = len(ATOM_LIST)
+num_chirality_tag = 3
+
+num_bond_type = 2 # self-connected and otherwise
+
+
+class GINEConv(MessagePassing):
+    def __init__(self, emb_dim, aggr="add"):
+        super(GINEConv, self).__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(emb_dim, 2*emb_dim), 
+            nn.ReLU(), 
+            nn.Linear(2*emb_dim, emb_dim)
+        )
+        self.edge_embedding1 = nn.Embedding(num_bond_type, emb_dim)
+        # self.edge_embedding2 = nn.Embedding(num_bond_direction, emb_dim)
+
+        nn.init.xavier_uniform_(self.edge_embedding1.weight.data)
+        # nn.init.xavier_uniform_(self.edge_embedding2.weight.data)
+        self.aggr = aggr
+
+    def forward(self, x, edge_index, edge_attr):
+        # add self loops in the edge space
+        edge_index = add_self_loops(edge_index, num_nodes=x.size(0))[0]
+
+        # add features corresponding to self-loop edges.
+        self_loop_attr = torch.ones(x.size(0)) # bond type for self-loop edge
+        # self_loop_attr[:,0] = 4
+        self_loop_attr = self_loop_attr.to(edge_attr.device).to(edge_attr.dtype)
+        edge_attr = torch.cat((edge_attr, self_loop_attr), dim=0)
+
+        # edge_embeddings = self.edge_embedding1(edge_attr[:,0]) + self.edge_embedding2(edge_attr[:,1])
+        edge_embeddings = self.edge_embedding1(edge_attr)
+
+        # return self.propagate(self.aggr, edge_index, x=x, edge_attr=edge_embeddings)
+        return self.propagate(edge_index, x=x, edge_attr=edge_embeddings)
+
+    def message(self, x_j, edge_attr):
+        return x_j + edge_attr
+
+    def update(self, aggr_out):
+        return self.mlp(aggr_out)
+
+
+class GINet(nn.Module):
+    """
+    Args:
+        num_layer (int): the number of GNN layers
+        emb_dim (int): dimensionality of embeddings
+        JK (str): last, concat, max or sum.
+        max_pool_layer (int): the layer from which we use max pool rather than add pool for neighbor aggregation
+        drop_ratio (float): dropout rate
+        gnn_type: gin, gcn, graphsage, gat
+    Output:
+        node representations
+    """
+    def __init__(self, num_layer=5, emb_dim=256, feat_dim=512, JK="last", drop_ratio=0):
+        super(GINet, self).__init__()
+        self.num_layer = num_layer
+        self.emb_dim = emb_dim
+        self.feat_dim = feat_dim
+        # self.num_class = num_class
+        self.num_class = num_class
+        self.drop_ratio = drop_ratio
+        self.JK = JK
+
+        if self.num_layer < 2:
+            raise ValueError("Number of GNN layers must be greater than 1.")
+
+        self.x_embedding1 = nn.Embedding(num_atom_type, emb_dim)
+        self.x_embedding2 = nn.Linear(3, emb_dim)
+        # self.x_embedding3 = nn.Linear(92, emb_dim)
+        # self.x_embedding2 = nn.Embedding(num_chirality_tag, emb_dim)
+
+        nn.init.xavier_uniform_(self.x_embedding1.weight.data)
+        # nn.init.xavier_uniform_(self.x_embedding2.weight.data)
+
+        # List of MLPs
+        self.gnns = nn.ModuleList()
+        for layer in range(num_layer):
+            self.gnns.append(GINEConv(emb_dim, aggr="add"))
+
+        # List of batchnorms
+        self.batch_norms = nn.ModuleList()
+        for layer in range(num_layer):
+            self.batch_norms.append(nn.BatchNorm1d(emb_dim))
+
+        self.feat_lin = nn.Linear(self.emb_dim, self.feat_dim)
+
+        self.pool = global_mean_pool
+
+        self.ft_layers = nn.Sequential(
+            nn.Linear(self.feat_dim, self.feat_dim//2), 
+            nn.Softplus(),
+            nn.Linear(self.feat_dim//2, 1)
+        )
+
+    def forward(self, data):
+        atomics = data.atomics
+        pos = data.pos
+        feat = data.feat
+        edge_index = data.edge_index
+        edge_attr = data.edge_attr
+        #print("mismatch")
+        h = self.x_embedding1(data.atomics) + self.x_embedding2(data.pos)
+        # h = self.x_embedding1(data.atomics) + \
+        #     self.x_embedding2(data.pos) + \
+        #     self.x_embedding3(data.feat)
+
+        for layer in range(self.num_layer):
+            h = self.gnns[layer](h, edge_index, edge_attr)
+            h = self.batch_norms[layer](h)
+            if layer == self.num_layer - 1:
+                h = F.dropout(h, self.drop_ratio, training=self.training)
+            else:
+                h = F.dropout(F.relu(h), self.drop_ratio, training=self.training)
+
+        # h = F.relu(h)
+        feat = self.feat_lin(h)
+        #print("mismatch")
+        feat = self.pool(feat, data.batch)
+        out = self.ft_layers(feat)
+        
+        return feat, out
+
+    def load_my_state_dict(self, state_dict):
+        own_state = self.state_dict()
+       # print(own_state)
+        for name, param in state_dict.items():
+            #print(name)
+            if name not in own_state:
+                #print(name)
+                continue
+            if isinstance(param, nn.parameter.Parameter):
+                #print(name)
+                # backwards compatibility for serialized parameters
+                param = param.data
+                print(param.size)
+            own_state[name].copy_(param)
+
+
+if __name__ == "__main__":
+    model = GINet()
+    print(model)
